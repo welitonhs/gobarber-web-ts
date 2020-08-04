4c18d4c95442bbe1efb84be1dcf214c3ee97fa3a
Weliton Sernajotto <weliton.sernajotto23@gmail.com>
2020-08-03 23:47:03 -0300

    Added hook to use AuthContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useCallback, useState } from 'react';
+import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
 
 interface AuthState {
@@ -20,9 +20,7 @@ interface AuthContextData {
   signIn(credentials: SignInCredentials): Promise<void>;
 }
 
-export const AuthContext = createContext<AuthContextData>(
-  {} as AuthContextData,
-);
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>(() => {
@@ -38,7 +36,6 @@ export const AuthProvider: React.FC = ({ children }) => {
 
     return {} as AuthState;
   });
-
   const signIn = useCallback(async ({ email, password }) => {
     const response = await api.post('sessions', { email, password });
     const { token, user } = response.data;
@@ -52,3 +49,11 @@ export const AuthProvider: React.FC = ({ children }) => {
     </AuthContext.Provider>
   );
 };
+
+export function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+}
